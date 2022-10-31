f56a51e860c1f373647127c9ee827a764b664e01
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2022-10-31 08:03:26 -0600

    over

---
+++ Proyecto 2/Proyecto 2/Game_Over.java
@@ -0,0 +1,19 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class Game_Over extends World
+{
+
+    /**
+     * Constructor for objects of class Game_Over.
+     */
+    public Game_Over()
+    {
+        super(600, 400, 1);
+    }
+}
