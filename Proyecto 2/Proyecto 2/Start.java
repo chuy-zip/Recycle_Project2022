d6fd73e222417b231ecbc3c62517d42774f91da0
FabiDeG <fabiola.d.g.12@gmail.com>
2022-11-02 08:09:14 -0600

    Start, botton of dog

--- Proyecto 2/Proyecto 2/Start.java
+++ Proyecto 2/Proyecto 2/Start.java
@@ -1,9 +1,9 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Start here.
+ * Here we show the strart page
  * 
- * @author (your name) 
+ * @author Fabi
  * @version (a version number or a date)
  */
 public class Start extends World
@@ -17,6 +17,17 @@ public class Start extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(640, 480, 1); 
-        
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Dog dog = new Dog();
+        addObject(dog, 250, 117);
+        dog.setLocation(488,325);
     }
 }
