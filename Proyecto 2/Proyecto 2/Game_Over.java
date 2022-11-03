b42773f74dbe6403687b1db529803a6b5e330142
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2022-11-02 20:29:10 -0600

    Game over functionality
    Game over Score, input, layout and menu dog

--- Proyecto 2/Proyecto 2/Game_Over.java
+++ Proyecto 2/Proyecto 2/Game_Over.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import javax.swing.JOptionPane;
 /**
  * Write a description of class Game_Over here.
  * 
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Game_Over extends World
 {
     private int FinalScore;
+    private String Name = null;
     /**
      * Constructor for objects of class Game_Over.
      * 
@@ -18,5 +19,29 @@ public class Game_Over extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
         FinalScore = score;
+        Name = JOptionPane.showInputDialog("Enter your Name");
+
+        prepare();
+    }
+
+    public void act()
+    {
+        if (Name == null)
+        {
+            Name = JOptionPane.showInputDialog("Enter your Name");
+        }
+
+        showText(""+ FinalScore, 290, 155);
+        showText(""+ Name, 290, 190);
+
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Dog dog = new Dog();
+        addObject(dog,506,314);
     }
 }
