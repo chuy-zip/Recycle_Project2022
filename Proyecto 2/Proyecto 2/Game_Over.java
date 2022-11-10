15103db3c3f254bb8507bf4af95be3111769d007
madrid7200 <106935787+madrid7200@users.noreply.github.com>
2022-11-09 20:14:01 -0600

    SQL Table and data Base
    SQL code and integration on Game Over Class, Implemented SQL connection and inert command for high Score

--- Proyecto 2/Proyecto 2/Game_Over.java
+++ Proyecto 2/Proyecto 2/Game_Over.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import javax.swing.JOptionPane;
+import java.sql.*;
 /**
  * Write a description of class Game_Over here.
  * 
@@ -10,6 +11,12 @@ public class Game_Over extends World
 {
     private int FinalScore;
     private String Name = null;
+    
+    private Connection conn;
+    private String db_server;
+    private String db_database;
+    private String db_username;
+    private String db_password;
     /**
      * Constructor for objects of class Game_Over.
      * 
@@ -45,4 +52,40 @@ public class Game_Over extends World
         Dog dog = new Dog();
         addObject(dog,506,314);
     }
+	
+    public Connection getConnection() throws SQLException 
+    {
+        String url = "jdbc:mysql://localhost:3306/reciclaje";
+        String username = "root";
+        String password = "";
+        
+        System.out.println("Connecting database...");
+        
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }
+        System.out.println("Connected to database");
+        return conn;
+    }
+    
+    public void addScore() {
+        Connection conn = getConnection();
+        
+         String sql = "INSERT INTO `table_leaderboard`(`id`, `name`, `score`)"
+        + " VALUES ( null, ?,?)";
+        
+        PreparedStatement Stmt = conn.prepareStatement(sql);
+        
+        Stmt.setString (2, Name);
+        Stmt.setInt (3, FinalScore);
+        
+        Stmt.execute();
+        
+        conn.close();
+
 }
+}
+
+
